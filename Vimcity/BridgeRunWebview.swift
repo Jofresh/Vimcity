8538f764738e6997eb8273b2ac13077fc8aa20ea
Josef Myers <josefmyers@josefs-mac-mini>
2016-09-29 09:46:41 -0400

    * last build failed - missing file
    DONE
    Alerts
    Links to actual webpages from profiles
    All Listings on home tab
    lower case font on Logo
    
    TODO
    Sample Retail, Studio, and Service Profile Pages
    Show fullscreen photos from profile pages
    All business type icons on map
    Filter results -

---
+++ Vimcity/BridgeRunWebview.swift
@@ -0,0 +1,44 @@
+//
+//  BridgeRunWebview.swift
+//  Vimcity
+//
+//  Created by Josef Myers on 9/29/16.
+//  Copyright © 2016 thatlevel. All rights reserved.
+//
+
+import UIKit
+
+class BridgeRunWebview: UIViewController {
+
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let URL = NSURL(string: "http://www.bridgerun.com")
+        
+        webView.loadRequest(NSURLRequest(URL: URL!))
+        
+    }
+    
+    @IBAction func backButtonDidTouch(sender: AnyObject) {
+        if(webView.canGoBack) {
+            //Go back in webview history
+            webView.goBack()
+        } else {
+            //Pop view controller to preview view controller
+            self.navigationController?.popViewControllerAnimated(true)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
